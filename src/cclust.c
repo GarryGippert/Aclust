a23c8947060cc3790513922b532d147f4c6a37af
Garry Gippert <92083616+GarryGippert@users.noreply.github.com>
2025-01-20 14:09:05 +0100

    code cleanup

--- src/cclust.c
+++ src/cclust.c
@@ -1,16 +1,21 @@
-/* ACLUST
+/* CCLUST - COLLAPSE CLUSTERS
 
-COLLAPSE CLUSTERS
+Compute NNJ binary tree from distance matrix and generate iTol datasets that
+collapse clusters.
 
-Input file is a distance matrix text file with three columns representing N points and N*(N+1)/2 distances.
-
+Input file is a distance matrix represented by a text file with three columns:
 	labelI labelI 0.0
 	labelI labelJ distanceIJ
 	labelJ labelJ 0.0
-	etc.
+	...
+Input should contain N different labels and all N*(N+1)/2 pairwise distances
+(including self).
 
-Output files produced: (all with shared prefix)
-	prefix_dree.txt	Distance matrix NNJ tree, Newick format.
+Command line parameters include:
+	-p 'prefix'			= prefix used for output files
+
+Output includes
+	<prefix>.dree.txt file		= NNJ tree, Newick format.
 
 */
 
@@ -23,85 +28,12 @@ Output files produced: (all with shared prefix)
 #define MAXFILENAME 1000
 #define MAXLINELEN 1000
 #define MAXWORDLEN 100
-#define DEFAULT_SCORE_MATRIX "dat/BLOSUM62.dat"
-
 #define	EPSILON		1.0e-8
 #define NEARZERO(a)	(fabs(a) < 10.0*EPSILON ? 0.0 : (a))
 #define SIGN(a)		( (a) < 0.0 ?   (-1.0) :    (1.0) )
 
 int p_v = 0;			/* verbose flag, set to 1 for additional diagnostic output */
-double p_go = 12.0;		/* gap open = first gap penalty */
-double p_ge = 1.0;		/* gap extend = next gap penalty */
-int p_json = 1;			/* output alignments in pseudo-json format */
-int p_nonself = 0;		/* do not align with self (show only off-diagonal elements */
-
-char *f_dmx = NULL;
 char *oprefix = NULL;
-
-FILE *jsnfp = NULL;		/* file pointer for writing alignment JSON line-by-line */
-FILE *alnfp = NULL;		/* file pointer for writing alignment free text */
-
-void j_osb(FILE * fp)
-/* output jsonified open square bracket */
-{
-	fprintf(fp, "[");
-}
-
-void j_cmt(FILE * fp, char *comment)
-/* output jsonified comment */
-{
-	if (comment)
-		fprintf(fp, ", \"comment\": \"%s\"", comment);
-}
-
-void j_url(FILE * fp, char *url)
-/* output jsonified url */
-{
-	if (url)
-		fprintf(fp, ", \"url\": \"%s\"", url);
-}
-
-#define NO 0
-#define YES 1
-
-void j_int(FILE * fp, int comma, char *key, int value, char *comment, char *url)
-/* output jsonified integer */
-{
-	fprintf(fp, "{\"key\": \"%s\", \"value\": %d", key, value);
-	j_cmt(fp, comment);
-	j_url(fp, url);
-	fprintf(fp, "}");
-	if (comma == YES)
-		fprintf(fp, ",");
-}
-
-void j_str(FILE * fp, int comma, char *key, char *value, char *comment, char *url)
-/* output jsonified string */
-{
-	fprintf(fp, "{\"key\": \"%s\", \"value\": \"%s\"", key, value);
-	j_cmt(fp, comment);
-	j_url(fp, url);
-	fprintf(fp, "}");
-	if (comma == YES)
-		fprintf(fp, ",");
-}
-
-void j_dbl(FILE * fp, int comma, char *key, double value, char *comment, char *url)
-/* output jsonified string */
-{
-	fprintf(fp, "{\"key\": \"%s\", \"value\": %g", key, value);
-	j_cmt(fp, comment);
-	j_url(fp, url);
-	fprintf(fp, "}");
-	if (comma == YES)
-		fprintf(fp, ",");
-}
-
-void j_csb(FILE * fp)
-/* output jsonified close square bracket */
-{
-	fprintf(fp, "]\n");
-}
 
 double *double_vector(int n)
 /* return double * vector of length n*/
@@ -309,18 +241,14 @@ int g_nent = 0, g_nsrc = 0;
 char *facc[MAXENTRIES];
 /* int  *frti[MAXENTRIES]; residue index deactivated */
 
-/* Global parameters start with p_ */
-int p_m = 0;			/* multiple alignment input */
-
 /* used in parsing text */
-char line[MAXLINELEN], text[MAXLINELEN], acc[MAXLINELEN], seq[MAXSEQUENCELEN];
+char line[MAXLINELEN], text[MAXLINELEN], acc[MAXLINELEN];
 
 int p_strict = 0;		/* set to 1, and no deviation is allowed in the recomputation of alignment score */
 
 /* EMBED */
 
 int p_dim = 20;			/* embed dimension: default 20 */
-char p_e = 'F';			/* 'D' = distance only, 'S' = single only, 'F' = full recursive embed */
 int p_ilim = 1000;		/* embed iteration limit: default 1000 */
 double p_clim = 1.0e-12;	/* embed polynomial convergence limit, default 1e-12 */
 
@@ -670,13 +598,13 @@ int bnode_count(BNODE * B)
 		fprintf(stderr, "B->left is NULL or B->right is NULL\n"), exit(1);
 	return bnode_count(B->left) + bnode_count(B->right);
 }
+
 int bnode_length(BNODE *B)
 {
 	return bnode_count(B);
 }
 
 int PRINTNL = 1;
-int PRINTPOS = 0;
 int PRINTDIS = 0;
 
 /* assign B->pos to weighted average of left and right trees */
@@ -709,19 +637,6 @@ void bnode_average_pos(BNODE * B)
 	B->dim = dim;
 }
 
-void printpos(FILE * fp, BNODE * B)
-{
-	if (!B->pos || !B->dim)
-		return;
-	int k;
-	fprintf(fp, "[%g", B->pos[0]);
-	for (k = 1; k < B->dim; k++)
-		fprintf(fp, ",%g", B->pos[k]);
-	fprintf(fp, "]");
-	if (PRINTNL)
-		fprintf(fp, "\n");
-}
-
 /* traverse binary tree
 */
 
@@ -742,16 +657,12 @@ void bnode_print_tree(FILE * fp, BNODE * B)
 	else if (B->left && B->right) {
 		/* left */
 		fprintf(fp, "(");
-		if (PRINTPOS)
-			printpos(fp, B->left);
 		bnode_print_tree(fp, B->left);
 		fprintf(fp, ":%g", B->left_distance);
 		if (PRINTNL)
 			fprintf(fp, "\n");
 		/* right */
 		fprintf(fp, ",");
-		if (PRINTPOS)
-			printpos(fp, B->right);
 		bnode_print_tree(fp, B->right);
 		fprintf(fp, ":%g", B->right_distance);
 		if (PRINTNL)
@@ -781,28 +692,8 @@ void bnode_indexi(BNODE * B, int *index, int *i)
 		fprintf(stderr, "bnode_indexi: B->left xor B->right in bnode_indexi\n"), exit(1);
 }
 
-void bnode_bnodei(BNODE * B, BNODE ** bnode, int *i)
-/* recursive assign ith value of bnode vector to tree-ordered leaf node
-// DO NOT DELETE the indirectly-allocated bnode in the usual way as it
-// might also delete parts of the tree it points to */
-{
-	if (B->index >= 0)
-		bnode[(*i)++] = B;
-	else if (B->left && B->right) {
-		bnode_bnodei(B->left, bnode, &(*i));
-		bnode_bnodei(B->right, bnode, &(*i));
-	}
-	else
-		fprintf(stderr, "bnode_bnodei: B->left xor B->right in bnode_bnodei\n"), exit(1);
-}
-
 #define DMX_ONE		0x00000001  /* == 1 */
 #define DMX_TWO		0x00000002  /* == 2 */
-#define DMX_THREE	0x00000004  /* == 4 */
-#define DMX_FOUR	0x00000008  /* == 8 */
-#define DMX_FIVE	0x00000010  /* == 16 */
-#define DMX_SIX		0x00000020  /* == 32 */
-#define DMX_SEVEN	0x00000040  /* == 64 */
 
 /* provide a binary tree from a DISTANCE matrix using nearest-neighbor joining algorithm and DISTANCE averaging (yuck) */
 /* dmx_flag should control distance calculation at nnj/node creation */
@@ -1164,192 +1055,6 @@ BNODE *bnode_tree(double **pos, int *index, int n, int dim)
 	return P;
 }
 
-void printdmx(FILE * fp, double **dmx, int *index, char **label, int n)
-{
-	fprintf(fp, "SUB DMX %d\n", n);
-	int i, j;
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < i; j++)
-			fprintf(fp, " %5s", "");
-		for (j = i; j < n; j++)
-			fprintf(fp, " %5.2f", dmx[i][j]);
-		fprintf(fp, " index %d label %s\n", index[i], label[i]);
-	}
-}
-
-void bnode_positional_distance_difference(BNODE * B, double **odmx)
-{
-	int n = bnode_count(B);
-	/* number of leaves in this subtree */
-
-	/* assign indices to local vector */
-	int *index = int_vector(n), i = 0, j;
-	bnode_indexi(B, index, &i);
-	if (i != n)
-		fprintf(stderr, "bnode_positional_distance_difference: after bnode_indexi i %d != n %d\n", i, n), exit(1);
-
-	/* assign bnode lookup vector */
-	BNODE **bnode;
-	bnode = bnode_vec(n, 0);/* allocate bnode pointer vector */
-	i = 0;
-	bnode_bnodei(B, bnode, &i);
-	if (i != n)
-		fprintf(stderr, "bnode_positional_distance_difference: after bnode_bnodei i %d != n %d\n", i, n), exit(1);
-	double sum = 0.0, cnt = 0.0, sqr = 0.0, ave, rms, ddmx, dpos, ddif;
-	for (i = 0; i < n; i++) {
-		for (j = i + 1; j < n; j++) {
-			ddmx = odmx[index[i]][index[j]];
-			dpos = bnode_dis(bnode[i], bnode[j]);
-			ddif = dpos - ddmx;
-			if (p_v > 1)
-				printf(" ij %d %d dpos %g ddmx %g ddif %g\n",
-				       i, j, dpos, ddmx, ddif);
-			sum += ddif;
-			sqr += ddif * ddif;
-			cnt += 1.0;
-		}
-	}
-	if (cnt > 0.0) {
-		ave = sum / cnt;
-		sqr = sqr / cnt;
-		rms = sqrt(sqr - ave * ave);
-	}
-	if (p_v > 1)
-		fprintf(stderr, "bnode_positional_distance_difference n= %d distance (positional - matrix) ave= %g rms= %g\n",
-			n, ave, rms);
-
-	free((char *)index);
-	free((char *)bnode);
-}
-
-void bnode_treebranch_distance_difference(BNODE * B, double **odmx)
-{
-	/* number of leaves in this subtree */
-	int n = bnode_count(B);
-
-	/* assign indices to local vector */
-	int *index = int_vector(n), i = 0, j;
-	bnode_indexi(B, index, &i);
-	if (i != n)
-		fprintf(stderr, "bnode_treebranch_distance_difference: after bnode_indexi i %d != n %d\n", i, n), exit(1);
-
-	/* assign bnode lookup vector */
-	BNODE **bnode;
-	bnode = bnode_vec(n, 0);/* allocate bnode pointer vector */
-	i = 0;
-	bnode_bnodei(B, bnode, &i);
-	if (i != n)
-		fprintf(stderr, "bnode_treebranch_distance_difference: after bnode_bnodei i %d != n %d\n", i, n), exit(1);
-
-	double sum = 0.0, cnt = 0.0, sqr = 0.0, ave, rms, ddmx, dbln, ddif;
-	for (i = 0; i < n; i++) {
-		for (j = i + 1; j < n; j++) {
-			ddmx = odmx[index[i]][index[j]];
-			dbln = bnode_dis_treebranch(bnode[i], bnode[j]);
-			ddif = dbln - ddmx;
-			if (p_v > 1)
-				printf(" ij %d %d dbln %g ddmx %g ddif %g\n",
-				       i, j, dbln, ddmx, ddif);
-			sum += ddif;
-			sqr += ddif * ddif;
-			cnt += 1.0;
-		}
-	}
-	if (cnt > 0.0) {
-		ave = sum / cnt;
-		sqr = sqr / cnt;
-		rms = sqrt(sqr - ave * ave);
-	}
-	free((char *)index);
-	free((char *)bnode);
-}
-
-/* Re-embed and return new subtree using original distance matrix */
-BNODE *bnode_reembed(BNODE * B, char br, double **odmx, int on, int dim)
-{
-
-	if (B->index >= 0)
-		return B;
-
-	bnode_positional_distance_difference(B, odmx);
-	bnode_treebranch_distance_difference(B, odmx);
-
-	int n = bnode_count(B);	/* number of leaves in this subtree */
-	if (p_v > 1)
-		fprintf(stderr, "bnode_reembed br=%c n= %d on= %d dim %d\n", br, n, on, dim);
-
-	if (p_v > 1)
-		bnode_print(stderr, B);
-
-	/* assign indices to local vector */
-	int *index = int_vector(n), i = 0, j;
-	bnode_indexi(B, index, &i);
-	if (i != n)
-		fprintf(stderr, "bnode_reembed, i %d != n %d after bnode_indexi\n", i, n), exit(1);
-
-	/* assign local distance matrix */
-	double **dmx = double_matrix(n, n);
-	for (i = 0; i < n; i++) {
-		dmx[i][i] = 0.0;
-		for (j = i + 1; j < n; j++)
-			dmx[i][j] = dmx[j][i] = odmx[index[i]][index[j]];
-	}
-
-	/* embed the new (sub)matrix */
-	double **pos = embed_dmx(n, dmx);
-	if (!pos)
-		fprintf(stderr, "bnode_reembed, pos is NULL\n"), exit(1);
-
-	if (p_v > 1) {
-		fprintf(stderr, "POS\n");
-		for (i = 0; i < n; i++) {
-			fprintf(stderr, "pos :");
-			for (j = 0; j < dim; j++)
-				fprintf(stderr, " %g", pos[i][j]);
-			fprintf(stderr, "\n");
-		}
-	}
-
-	/* generate a subtree from the new positions */
-	BNODE *P;
-	P = bnode_tree(pos, index, n, dim);
-
-	if (p_v > 1) {
-		fprintf(stderr, "after reembedn\n");
-		bnode_print(stderr, P);
-	}
-
-	/* transfer information */
-	P->parent = B->parent;
-	P->parent_distance = B->parent_distance;
-
-	/* free old subtree */
-	bnode_free(B);
-
-	/* connect */
-	if (P->parent) {
-		if (br == 'L')
-			P->parent->left = P;
-		else if (br == 'R')
-			P->parent->right = P;
-		else
-			fprintf(stderr, "bnode_reembed, unknown branch rotation br '%c'\n", br), exit(1);
-	}
-	/* note - do not change P->parent->{left,right}_distance // as they are determined by a 'higher' level
-	   embedding // recursively apply this to left and right subbranches */
-
-	P->left = bnode_reembed(P->left, 'L', odmx, on, dim);
-	P->right = bnode_reembed(P->right, 'R', odmx, on, dim);
-
-	double_matrix_free(n, n, dmx);
-	double_matrix_free(n, dim, pos);
-	int_vector_free(n, index);
-
-	/* return new subtree */
-	return P;
-
-}
-
 void write_tree(BNODE * P, char *filename)
 {
 	FILE *fp;
@@ -1361,60 +1066,6 @@ void write_tree(BNODE * P, char *filename)
 	fclose(fp);
 }
 
-void write_tree_binary(BNODE * P, char *filename)
-{
-	fprintf(stderr, "write_tree_binary stub bnode %d, filename %s\n", bnode_length(P), filename);
-	return ;
-
-	printf("You are attempting to write_tree_binary to %s, unsuccessfully! treelength %d\n", filename, bnode_count(P));
-	fprintf(stderr, "You are attempting to write_tree_binary %s, unsuccessfully! treelength %d\n", filename, bnode_count(P));
-	exit(1);
-}
-
-void write_dmx(double **dmx, char *oprefix)
-/* print distance upper half matrix plus diagonal */
-{
-	char *filename = char_vector(strlen(oprefix) + strlen(".dmx.txt") + 1);
-	sprintf(filename, "%s%s", oprefix, ".dmx.txt");
-	FILE *fp;
-	int i, j, c;
-	if ((fp = fopen(filename, "w")) == NULL)
-		fprintf(stderr, "Distance file %s cannot be opened for writing\n", filename), exit(1);
-	for (c = 0, i = 0; i < g_nent; i++)
-		for (j = i; j < g_nent; j++, c++)
-			fprintf(fp, "%s %s %f\n", facc[i], facc[j], dmx[i][j]);
-	fclose(fp);
-	fprintf(stderr, "Wrote dmx %s %d elements, expect N(N+1)/2 %d for N=%d\n",
-		filename, c, g_nent*(g_nent+1)/2, g_nent);
-	free(filename);
-}
-
-BNODE *bnode_distance_tree(int n, double **dmx)
-{
-	int *index = int_vector_ramp(n);
-	int dmx_flag = DMX_ONE;
-	fprintf(stderr, "bnode_distance_tree dmx_flag=%d\n", dmx_flag);
-	BNODE *P = bnode_tree_dmx(n, index, dmx, dmx_flag);
-	return(P);
-}
-/* After
-// 1. alignments have now provided a distance matrix
-// The the task of tree building can begin
-// 2. embed distance matrix into orthogonal coordinates,
-// 3. build binary tree based on nearest neighbor-joining algorithm.
-// 4. recursively visit each sub-branch and redo embed+tree (steps 2 and 3).
-// Garry Paul Gippert.
-*/
-
-BNODE *bnode_embed_tree(int n, double **dmx)
-{
-	double **pos = embed_dmx(n, dmx);
-	int *index = int_vector_ramp(n);
-	BNODE *P = bnode_tree(pos, index, n, p_dim);
-	int_vector_free(n, index);
-	return P;
-}
-
 #define COMMAND_LINE_HELP "\n\n\
 CCLUST  Collapse clusters from distance matrix and NNJ tree.\n\
 Required parameters:\n\
@@ -1424,19 +1075,10 @@ Optional parameters:\n\
 	-e <char>		(D) distance tree only, (S) distance+single embed trees, (F, default) distance+single+full embed trees\n\
 	-go <float>		Gap open penalty\n\
 	-ge <float>		Gap extend penalty\n\
-Optional flags:\n\
-	-m 			activates to interpret input Fasta as MSA\n\
-Less important flags:\n\
-	-j			deactivates writing of JSON alignment file\n\
-	-nonself		deactivates self alignments\n\
 	-v			activates more verbose output\n\
 \n\
-Input files are Fasta with >accession on one line and sequences on following until the next > is reached\n\
-Output files share a prefix <p>, which is default name of first fasta input file\n\
-	<p>_aln.txt	alignments, text\n\
-	<p>_aln.js	alignments individual JSON rows (optional, activate using -j) \n\
-	<p>_dmx.txt	distance matrix, text\n\
-	<p>_dree.txt	distance matrix tree, newick text\n\
+Output files share a common prefix <p>, which is default name of first fasta input file\n\
+	<p>_dree.txt	DMX NNJ binary tree, Newick format\n\
 	<p>_tree0.txt	single embed tree, newick text\n\
 	<p>_tree.txt	fulfull embed tree, newick text\n\
 \n\
@@ -1463,18 +1105,13 @@ void parameter_value_missing(int cstart, int argc, char *argv[])
 	fprintf(stderr, "value needed after parameter [%d] '%s', try '%s -h' for HELP\n",
 		c, argv[c], argv[0]), exit(1);
 }
+
 int pparse(int argc, char *argv[])
 /* parse command line and set some input and output filename defaults */
 {
 	int c = 1;
 	while (c < argc) {
-		if (strncmp(argv[c], "-d", 2) == 0) {
-			if (++c == argc)
-				parameter_value_missing(c, argc, argv);
-			f_dmx = char_string(argv[c++]);
-			fprintf(stderr, "f_dmx set to '%s'\n", f_dmx);
-		}
-		else if (strncmp(argv[c], "-h", 2) == 0) {
+		if (strncmp(argv[c], "-h", 2) == 0) {
 			++c;
 			command_line_help(c, argc, argv);
 		}
@@ -1483,48 +1120,6 @@ int pparse(int argc, char *argv[])
 				parameter_value_missing(c, argc, argv);
 			oprefix = char_string(argv[c++]);
 			fprintf(stderr, "output prefix set to '%s'\n", oprefix);
-		}
-		else if (strncmp(argv[c], "-e", 2) == 0) {
-			if (++c == argc)
-				parameter_value_missing(c, argc, argv);
-			if (sscanf(argv[c], "%c", &p_e) == 1)
-				fprintf(stderr, "Embed set to %d\n", p_e);
-			else
-				fprintf(stderr, "Could not parse argv[%d] '%s'\n", c, argv[c]), exit(1);
-			c++;
-		}
-		else if (strncmp(argv[c], "-go", 3) == 0) {
-			if (++c == argc)
-				parameter_value_missing(c, argc, argv);
-			if (sscanf(argv[c], "%lf", &p_go) == 1)
-				fprintf(stderr, "Gap open set to %g\n", p_go);
-			else
-				fprintf(stderr, "Could not parse argv[%d] '%s'\n", c, argv[c]), exit(1);
-			c++;
-		}
-		else if (strncmp(argv[c], "-ge", 3) == 0) {
-			if (++c == argc)
-				parameter_value_missing(c, argc, argv);
-			if (sscanf(argv[c], "%lf", &p_ge) == 1)
-				fprintf(stderr, "Gap extension set to %g\n", p_ge);
-			else
-				fprintf(stderr, "Could not parse argv[%d] '%s'\n", c, argv[c]), exit(1);
-			c++;
-		}
-		else if (strncmp(argv[c], "-nonself", 8) == 0) {
-			++c;
-			p_nonself = (p_nonself + 1) % 2;
-			fprintf(stderr, "nonself flag set to %d\n", p_nonself);
-		}
-		else if (strncmp(argv[c], "-j", 2) == 0) {
-			++c;
-			p_json = (p_json + 1) % 2;
-			fprintf(stderr, "json flag set to %d\n", p_json);
-		}
-		else if (strncmp(argv[c], "-m", 2) == 0) {
-			++c;
-			p_m = 1;
-			fprintf(stderr, "multiple align set to %d\n", p_m);
 		}
 		else if (strncmp(argv[c], "-v", 2) == 0) {
 			++c;
@@ -1542,9 +1137,6 @@ int pparse(int argc, char *argv[])
 		}
 
 	}
-	/* some validation */
-	if (strchr("DSF", p_e) == NULL)
-		fprintf(stderr, "Parameter -e %c invalid, must be D, S or F\n", p_e), exit(1);
 
 	/* set some variables */
 	if (!oprefix) {
@@ -1567,9 +1159,10 @@ double **read_dmx(char *filename)
 /* usage double **dmx = read_dmx(f_dmx); */
 /* remember MAXENTRIES 10000 int g_nent = 0; char *facc[MAXENTRIES]; */
 {
+	fprintf(stderr, "Attempt read DMX from '%s'\n", filename);
 	double **dmx = NULL;
-	int i, j;
 	g_nent = 0;
+	int i, j;
 	char line[MAXLINELEN], word1[MAXWORDLEN], word2[MAXWORDLEN];
 	float value;
 	int index1, index2;
@@ -1727,14 +1320,19 @@ void bnode_split(BNODE *B, int n, double **dmx)
 int main(int argc, char *argv[])
 {
 	int c = pparse(argc, argv);
+	int dmx_flag = DMX_ONE;
+	char *f_dmx = char_string(argv[c]);
 
-	/* read_dmx returns double ** but also sets g_nent and point labels facc */
+	/* read double **dmx, but also set global variables containing
+	   point labels and count, char **facc and g_nent, respectively */
 	double **dmx = read_dmx(f_dmx);
 
-	/* binary NNJ tree */
-	BNODE *dree = bnode_distance_tree(g_nent, dmx);
+	/* construct DMX NNJ binary tree */
+	int *index = int_vector_ramp(g_nent);
+	fprintf(stderr, "bnode_tree_dmx dmx_flag=%d\n", dmx_flag);
+	BNODE *dree = bnode_tree_dmx(g_nent, index, dmx, dmx_flag);
 
-	/* write binary NNJ tree */
+	/* write DMX NNJ binary tree */
 	char *treefile = NULL;
 	treefile = char_vector(strlen(oprefix) + strlen(".dree.txt") + 1);
 	sprintf(treefile, "%s%s", oprefix, ".dree.txt");
@@ -1744,34 +1342,6 @@ int main(int argc, char *argv[])
 
 	bnode_split(dree, g_nent, dmx);
 
-	exit(0);
-
-	if (p_e == 'D')
-		fprintf(stderr, "Halt after distance tree\n"), exit(0);
-
-	/* Construct tree based on single pass embedding.
-	*/
-	BNODE *tree = bnode_embed_tree(g_nent, dmx);
-	treefile = char_vector(strlen(oprefix) + strlen(".tree0.txt") + 1);
-	sprintf(treefile, "%s%s", oprefix, ".tree0.txt");
-	fprintf(stderr, "Single embed tree %s\n", treefile);
-	write_tree(tree, treefile);
-	free(treefile);
-	if (p_e == 'S')
-		fprintf(stderr, "Halt after single embed tree\n"), exit(0);
-
-	/* Continue with full recursive embedding.
-	*/
-	tree->left = bnode_reembed(tree->left, 'L', dmx, g_nent, p_dim);
-	tree->right = bnode_reembed(tree->right, 'R', dmx, g_nent, p_dim);
-	treefile = char_vector(strlen(oprefix) + strlen(".tree.txt") + 1);
-	sprintf(treefile, "%s%s", oprefix, ".tree.txt");
-	fprintf(stderr, "Full recursive embed tree %s\n", treefile);
-	write_tree(tree, treefile);
-	free(treefile);
-
-	char *binary_treefile = char_string("binary_treefile.btf");
-	write_tree_binary(tree, binary_treefile);
-
+	int_vector_free(g_nent, index);
 	exit(0);
 }
