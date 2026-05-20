bf1232dff34227d607e3025568f8136a2c24328e
Garry Gippert <92083616+GarryGippert@users.noreply.github.com>
2026-05-20 13:46:04 +0200

    additional tests

--- test/set01/dorte.py
+++ test/set01/dorte.py
@@ -9,12 +9,19 @@ def doc(cmd):
 
 cmd = "../../src/bclust -p one -alf AA.aln AB.aln BB.aln > one.out 2> one.err"
 doc(cmd)
+print ("bclust runs with several ALF input files")
+
+cmd = "diff one.dree.txt expect.dree.txt"
+doc(cmd)
+print ("bclust produces expected output with several ALF input files")
 
 cmd = "cat AA.aln AB.aln BB.aln > AAABBB.aln"
 doc(cmd)
 
 cmd = "../../src/bclust -p two -alf AAABBB.aln > two.out 2> two.err"
 doc(cmd)
+print ("bclust runs with concatenated ALF input")
 
-cmd = 'diff one.dree.txt two.dree.txt'
+cmd = 'diff two.dree.txt expect.dree.txt'
 doc(cmd)
+print ("bclust produces expected output with concatenated ALF input")
